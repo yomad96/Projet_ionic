d7d491423761520c019d32a53706dcd5375100f5
BeatlesBOB <38206766+BeatlesBOB@users.noreply.github.com>
2020-05-12 16:35:07 +0200

    Map poly

--- ionic/src/app/map/map.page.ts
+++ ionic/src/app/map/map.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
-import {GameService} from "../services/game.service";
+import {GameService} from '../services/game.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -13,14 +14,17 @@ export class MapPage implements OnInit {
   // @ts-ignore
   markPoint: L.marker;
   polyline: L.polyline;
-
   reponse: boolean;
+  validate : boolean = false;
   reponseLat: number;
   reponseLng: number;
+  distance: number;
+  point: number;
+  pourcentage: any;
 
   ionViewDidEnter() { this.leafletMap(); }
 
-  constructor(gameService: GameService) { }
+  constructor(private gameService: GameService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -33,27 +37,30 @@ export class MapPage implements OnInit {
       attribution: 'edupala.com'
     }).addTo(this.map);
 
-
-
-
     this.map.on('click', e => {
-
 
       if (this.markPoint !== undefined) {
         this.map.removeLayer(this.markPoint);
         this.reponse = false;
 
       }
-
-      //Add a marker to show where you clicked.
-      this.markPoint = L.marker([e.latlng.lat,e.latlng.lng]).addTo(this.map);
+      // Add a marker to show where you clicked.
+      this.markPoint = L.marker([e.latlng.lat, e.latlng.lng]).addTo(this.map);
       this.reponse = true;
       // @ts-ignore
       this.reponseLat = e.latlng.lat;
       // @ts-ignore
       this.reponseLng = e.latlng.lng;
 
+    });
 
+    const southWest = L.latLng(-89.98155760646617, -180),
+        northEast = L.latLng(89.99346179538875, 180);
+    const bounds = L.latLngBounds(southWest, northEast);
+
+    this.map.setMaxBounds(bounds);
+    this.map.on('drag', () => {
+      this.map.panInsideBounds(bounds, { animate: false });
     });
   }
 
@@ -61,14 +68,13 @@ export class MapPage implements OnInit {
     this.map.remove();
   }
 
-  repondre(){
+  repondre() {
 
+    this.map.tap.disable();
     const poly = [
       [45.51, -122.68],
       [this.reponseLat, this.reponseLng]
     ];
-
-
 
     // @ts-ignore
     this.polyline = L.polyline(poly, {color: 'red'}).addTo(this.map);
@@ -87,12 +93,18 @@ export class MapPage implements OnInit {
         Math.sin(difflng / 2) * Math.sin(difflng / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
-    const d = (R * c) / 1000; // in metres
+    this.distance = (R * c) / 1000;
 
-    console.log('Distance : ' + d+ " Kilomètres");
+    this.point = Math.round(5000 - ( this.distance / 40000) * 5000);
+    this.gameService.addPoint(this.point);
+    this.pourcentage = (this.point * 100 / 5000) / 100;
+
+    this.validate = true;
 
   }
 
-
+  suivant() {
+    this.router.navigate(['/resultat-question']);
+  }
 
 }
--- ionic/src/app/services/game.service.ts
+++ ionic/src/app/services/game.service.ts
@@ -5,5 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class GameService {
 
+  private point: number = 0;
+
   constructor() { }
+
+  addPoint(point: number) {
+    this.point = this.point = point;
+  }
 }
