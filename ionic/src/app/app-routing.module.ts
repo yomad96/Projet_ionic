d015046c144305d677e600e2f03e07a0c29abfe7
hbaral23 <baral.hugoBTSSN@gmail.com>
2020-05-12 16:35:51 +0200

    add localstorage scores | add countdown

--- ionic/src/app/app-routing.module.ts
+++ ionic/src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'scores',
+    loadChildren: () => import('./scores/scores.module').then( m => m.ScoresPageModule)
   },
+
 ];
 
 @NgModule({
