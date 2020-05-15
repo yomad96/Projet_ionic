ae2aa3aecfe1abf1fc7bf87f0c3bd303eac388eb
BeatlesBOB <38206766+BeatlesBOB@users.noreply.github.com>
2020-05-15 16:39:58 +0200

    resultat-question

--- ionic/src/app/result/result.page.ts
+++ ionic/src/app/result/result.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {GameService} from "../services/game.service";
-import {NavigationExtras, Router} from "@angular/router";
+import {GameService} from '../services/game.service';
+import {NavigationExtras, Router} from '@angular/router';
 
 @Component({
   selector: 'app-result',
--- ionic/src/app/services/api.service.ts
+++ ionic/src/app/services/api.service.ts
@@ -67,8 +67,11 @@ export class ApiService {
         this.start = "&start="+start;
     }
 
+    getspecfiqueApi(): Observable<any> {
+        return this.http.get(this.baseUrl + this.dataSet + this.recordid + "&apikey=de2038a4a0bf2045be261bd1e126211159c87bfdf4091daa4ceb8960");
+    }
     getUrlApi()
     {
-        return this.baseUrl+this.dataSet+this.lang+this.rows+this.start+this.refine;
+        return this.baseUrl + this.dataSet + this.recordid + this.lang + this.rows + this.start+this.refine+"&apikey=de2038a4a0bf2045be261bd1e126211159c87bfdf4091daa4ceb8960";
     }
 }
